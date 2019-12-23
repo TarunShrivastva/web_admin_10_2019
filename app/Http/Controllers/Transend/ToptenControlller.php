ab34bd7d35b89fcdbec571a1b2cb0f40fe9d7d1f
TarunShrivastva <shrivastvatarun@gmail.com>
2019-12-23 19:06:57 +0530

    compare fix

--- app/Http/Controllers/Transend/ToptenControlller.php
+++ app/Http/Controllers/Transend/ToptenControlller.php
@@ -54,17 +54,36 @@ class ToptenController extends Controller
                 $q->where('compare_products.status','1');
             })->with(['compareproduct.productOne.add_specification.specification','compareproduct.productTwo.add_specification.specification'])->get();
         
+        $actual_data = $compArray = array();
         foreach ($compares as $compare){
-            foreach ($compare->compareproduct as $product){
-                $actual_data = $compArray = $specsProductOne = $specsProductTwo = $keyArrayOne = $keyArrayTwo = array();
-                foreach($product->productOne->add_specification as $key1 => $value1){
-                    array_push($specsProductOne, [$value1->specification->title => $value1->value]);
+            foreach ($compare->compareproduct->toArray() as $key => $value) {
+                $productImageOne = $productImageTwo = $productTitleTwo = $productTitleOne = $productFlipkartOne = $productPaytmOne = $productAmazonOne = $productTataCliqOne = $productFlipkartTwo = $productPaytmTwo = $productAmazonTwo = $productTataCliqTwo = 'N/A';
+                $actual_data_new = $compArrayNew = $specsProductOne = $specsProductTwo = $keyArrayOne = $keyArrayTwo = array();
+                if(isset($value['product_one']['title'])){
+                    $productTitleOne = $value['product_one']['title'];
+                    $productImageOne = $value['product_one']['image'];
+                    $productFlipkartOne = $value['product_one']['flipkart_price'];
+                    $productPaytmOne = $value['product_one']['paytm_price'];
+                    $productAmazonOne = $value['product_one']['amazon_price'];
+                    $productTataCliqOne = $value['product_one']['tata_cliq_price'];
                 }
-
-                foreach($product->productTwo->add_specification as $key2 => $value2){
-                    array_push($specsProductTwo, [ $value2->specification->title => $value2->value]);
+                if(isset($value['product_two']['title'])){
+                    $productTitleTwo = $value['product_two']['title'];    
+                    $productImageTwo = $value['product_two']['image'];
+                    $productFlipkartTwo = $value['product_two']['flipkart_price'];
+                    $productPaytmTwo = $value['product_two']['paytm_price'];
+                    $productAmazonTwo = $value['product_two']['amazon_price'];
+                    $productTataCliqTwo = $value['product_two']['tata_cliq_price'];
                 }
                 
+                foreach($value['product_one']['add_specification'] as $key1 => $value1){
+                    array_push($specsProductOne, [$value1['specification']['title'] => $value1['value']]);
+                }
+                
+                foreach($value['product_two']['add_specification'] as $key2 => $value2){
+                    array_push($specsProductTwo, [ $value2['specification']['title'] => $value2['value']]);
+                }
+
                 foreach ($specsProductOne as $product){
                     $key = array_keys($product);
                     array_push($keyArrayOne, $key[0]);
@@ -97,38 +116,17 @@ class ToptenController extends Controller
                         }
                     }    
                 }
-                $actual_data = $compArray = array();
-                foreach ($compare->compareproduct->toArray() as $key => $value) {
-                    $productImageOne = $productImageTwo = $productTitleTwo = $productTitleOne = $productFlipkartOne = $productPaytmOne = $productAmazonOne = $productTataCliqOne = $productFlipkartTwo = $productPaytmTwo = $productAmazonTwo = $productTataCliqTwo = 'N/A';
-                    if(isset($value['product_one']['title'])){
-                        $productTitleOne = $value['product_one']['title'];
-                        $productImageOne = $value['product_one']['image'];
-                        $productFlipkartOne = $value['product_one']['flipkart_price'];
-                        $productPaytmOne = $value['product_one']['paytm_price'];
-                        $productAmazonOne = $value['product_one']['amazon_price'];
-                        $productTataCliqOne = $value['product_one']['tata_cliq_price'];
-                    }
-                    if(isset($value['product_two']['title'])){
-                        $productTitleTwo = $value['product_two']['title'];    
-                        $productImageTwo = $value['product_two']['image'];
-                        $productFlipkartTwo = $value['product_two']['flipkart_price'];
-                        $productPaytmTwo = $value['product_two']['paytm_price'];
-                        $productAmazonTwo = $value['product_two']['amazon_price'];
-                        $productTataCliqTwo = $value['product_two']['tata_cliq_price'];
-                    }
-                    $actual_data = [
-                            'object' => $compare,
-                            'title' => [$productTitleOne, $productTitleTwo],
-                            'image' => [$productImageOne, $productImageTwo],
-                            'specifications' => $specsProductOne,
-                            'flipkart' => [$productFlipkartOne, $productFlipkartTwo],
-                            'paytm' => [$productPaytmOne, $productPaytmTwo],
-                            'amazon' => [$productAmazonOne, $productAmazonTwo],
-                            'tata_cliq' => [$productTataCliqOne, $productTataCliqTwo],
-                        ];
-                    array_push($compArray, $actual_data);
-                }
-                
+                $actual_data = [
+                        'object' => $compare,
+                        'title' => [$productTitleOne, $productTitleTwo],
+                        'image' => [$productImageOne, $productImageTwo],
+                        'specifications' => $specsProductOne,
+                        'flipkart' => [$productFlipkartOne, $productFlipkartTwo],
+                        'paytm' => [$productPaytmOne, $productPaytmTwo],
+                        'amazon' => [$productAmazonOne, $productAmazonTwo],
+                        'tata_cliq' => [$productTataCliqOne, $productTataCliqTwo],
+                    ];
+                array_push($compArray, $actual_data);   
             }
         }
         return view('transend.top-products.toptenCompare',compact('compArray'));
